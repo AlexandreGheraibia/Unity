c2a9c6315fa6c8c6cab77548c36c6c2dc58bc396
gheraibia <gheraibia@hotmail.com>
2020-11-29 20:20:53 +0100

    Add files via upload

---
+++ Prototype 1 Script/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject vehicule;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.parent = vehicule.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }   
+}
---
+++ Prototype 1 Script/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed,turnSpeed;
+    private float coeffY, fowardInput, brake=-1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        turnSpeed = 60f;
+        speed = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coeffY = Input.GetAxis("Horizontal") * turnSpeed;
+        fowardInput = Input.GetAxis("Vertical");
+       
+        if (fowardInput > 0.0f)
+        {
+            if (speed+ fowardInput > 50){
+                speed = 50;
+            }
+            else {
+                speed += fowardInput;
+            }
+        }
+        else {
+            if (fowardInput != 0.0f)
+            {
+                if (speed + brake > 0.0f)
+                    speed += brake;
+                else
+                {
+                    speed += fowardInput;
+                }
+            }
+           
+        }
+    	
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Rotate(Vector3.up * Time.deltaTime* coeffY);
+    }
+}
