a80d544d42c419c994a3c869d483a6837d90ad65
gheraibia <gheraibia@hotmail.com>
2020-11-29 20:23:38 +0100

    Add files via upload

---
+++ Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 10, -30);
+        transform.Rotate(Vector3.up*90);
+        transform.LookAt(plane.transform.position);
+        transform.parent = plane.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Challenge 1/Scripts/RotatePropeller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class RotatePropeller : MonoBehaviour
+{
+    public string objectName;
+     GameObject propeller=null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform propellerTransform = transform.GetComponentsInChildren<Transform>()
+                           .Where(child => objectName.Equals(child.gameObject.name))
+                           .DefaultIfEmpty(null).First();
+        if(propellerTransform!=null)
+            propeller=propellerTransform.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   //excepetion if propeller is null
+        propeller.transform.Rotate(30 * Vector3.forward, Space.Self);
+    }
+}
