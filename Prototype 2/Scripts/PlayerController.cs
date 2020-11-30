993b5cae4f20074995c5b5efc50657a000abb98c
gheraibia <gheraibia@hotmail.com>
2020-11-30 22:29:55 +0100

    Add files via upload

---
+++ Prototype 2/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float xInput;
+    public float speed;
+    public float[] Bounds = { -20.0f, 20.0f };
+    public GameObject projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //retrieves player input
+        xInput = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        //keep the player in bounds
+        if (transform.position.x + xInput >= Bounds[0] && transform.position.x + xInput <= Bounds[1])
+        {
+
+            transform.Translate(xInput, 0, 0);
+        }
+        //launch a projectilPrefabs from the player
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab,transform.position,transform.rotation);
+        }
+    }
+}
---
+++ Prototype 2/Scripts/SpwanManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SpwanManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    public float[] bounds= {-20,20 };
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimals", 2f, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnRandomAnimals() {
+        int i = Random.Range(1, 4); ;
+        animalPrefabs.Take(i).ToList().ForEach(animal => {
+            var x = Random.Range(bounds[0], bounds[1]);
+            Instantiate(animal, new Vector3(x, 0, 20), animal.transform.rotation);
+        });
+    }
+}
