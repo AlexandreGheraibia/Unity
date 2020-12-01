6bcad1c75dea88db1306b1fa78d9a7bb9ea9d588
gheraibia <gheraibia@hotmail.com>
2020-12-01 23:01:22 +0100

    Add files via upload

---
+++ Challenge 2/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    float spendTime=0;
+    // Update is called once per frame
+    void Update()
+    {
+         spendTime+=Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&& spendTime > 1.0f)
+        {
+            spendTime = 0;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
