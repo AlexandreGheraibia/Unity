ee6c8c3e8e1c935f6f84e1a06a8307d02650dd9f
gheraibia <gheraibia@hotmail.com>
2020-12-08 00:04:23 +0100

    Add files via upload

---
+++ Challenge You Have Been Named/YouHaveBeenNamed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class YouHaveBeenNamed : MonoBehaviour
+{
+    private List<string> names= new List<string>{"John","James","Jane","Alex","Bob"};
+    // Start is called before the first frame update
+    void Start()
+    {
+        names.ForEach(n => Debug.Log(n));
+        int named = Random.Range(0, names.Count);
+        names.RemoveAt(named);
+        names.ForEach(n => Debug.Log(n));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
