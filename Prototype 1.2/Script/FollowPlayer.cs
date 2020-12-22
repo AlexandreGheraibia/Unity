5765d052537ff77add5f0d2a6a637cbda83b9d29
gheraibia <gheraibia@hotmail.com>
2020-12-22 03:39:39 +0100

    Add files via upload

---
+++ Prototype 1.2/Script/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject vehicule;
+    // Start is called before the first frame update
+    //Awake method run when the object is create. Whereas 
+    //start method run when the scene is create.
+    void Start()
+    {
+        transform.parent = vehicule.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }   
+}
---
+++ Prototype 1.2/Script/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Linq;
+public class PlayerController : MonoBehaviour
+{
+    private const float wheelPerimeter=(2*0.15f*Mathf.PI),
+                        turnSpeed = 45f;
+    private TextMeshProUGUI speedText, rpmText;
+    [SerializeField] private float horsePower = 0;
+    public GameObject centerOfMass;
+    private  Rigidbody playerRB;
+    private float coeffY, fowardInput, speed;
+    private List<WheelCollider> wheelColliders;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        playerRB.centerOfMass = centerOfMass.transform.position;
+        speedText = GameObject.Find("Speedometer").GetComponent<TextMeshProUGUI>();
+        rpmText = GameObject.Find("RPM").GetComponent<TextMeshProUGUI>();
+        wheelColliders = GetComponentsInChildren<WheelCollider>().ToList();
+    }
+   
+    // Update is called once per frame
+    //appelé avant Update, sert pour gérer tout ce qui est mouvement
+    //ou événement physique.
+    void FixedUpdate()
+    {
+        if (IsOnGround())
+        {
+            coeffY = Input.GetAxis("Horizontal") * turnSpeed;
+            fowardInput = Input.GetAxis("Vertical");
+            playerRB.AddRelativeForce(Vector3.forward * horsePower * fowardInput, ForceMode.Force);
+            transform.Rotate(Vector3.up * Time.deltaTime * coeffY);
+            speed = playerRB.velocity.magnitude;
+            speedText.text = "Speed: " + Mathf.Round(speed * 3.6f) + "km/H";
+            rpmText.text = "RPM  " + Mathf.Round(speed * 60 / wheelPerimeter) + "T/M";
+        }
+      
+    }
+
+    bool IsOnGround()
+    {
+        return wheelColliders.Where(wheel => wheel.isGrounded).Count()==4;
+    }
+}
